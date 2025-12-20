46932908f4b4fe4762d6cfc71c1e0eaf07b7196a
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-20 10:02:07 +0530

    If Else Demo

---
+++ src/Control_Statements/IfElseDemo.java
@@ -0,0 +1,14 @@
+package Control_Statements;
+
+public class IfElseDemo {
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        if (number % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+    }
+}
