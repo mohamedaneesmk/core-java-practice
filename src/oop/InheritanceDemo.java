b031cd49569ef7551a87f92834cbe6e77f977f6e
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-06 18:17:29 +0530

    Inheritance Code

--- src/oop/InheritanceDemo.java
+++ src/oop/InheritanceDemo.java
@@ -2,7 +2,7 @@ class Animal {
     void eat() { System.out.println("Eating..."); }
 }
 class Dog extends Animal {
-    void bark() { System.out.println("Barking..."); }
+    void bark() { System.out.println("Barking..."); } 
 }
 public class InheritanceDemo {
     public static void main(String[] args) {
