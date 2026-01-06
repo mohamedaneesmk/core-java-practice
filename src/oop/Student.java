12b1a51c62fdb1d92162f2acb1a208b94d54ce4e
M K <2393201@cognizant.com>
2026-01-06 18:08:29 +0530

    Classes Objects

---
+++ src/oop/Student.java
@@ -0,0 +1,16 @@
+class Student {
+    String name;
+    int age;
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+public class ClassesObjects {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Alice";
+        s1.age = 20;
+        s1.display();
+    }
+}
