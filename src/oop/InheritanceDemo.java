609ed6816a056d3ea215d218e99406f5c886bf1b
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-06 18:15:39 +0530

    Inheritance

--- src/oop/InheritanceDemo.java
+++ src/oop/InheritanceDemo.java
@@ -7,7 +7,7 @@ class Dog extends Animal {
 public class InheritanceDemo {
     public static void main(String[] args) {
         Dog d = new Dog();
-        d.eat();
+        d.eat(); 
         d.bark();
     }
-}
+}     
