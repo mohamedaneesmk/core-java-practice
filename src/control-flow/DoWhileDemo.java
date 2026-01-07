8d7060112110c4ee52fa46e6bf860c36ada650cf
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-07 16:02:33 +0530

    Do While code

--- src/control-flow/DoWhileDemo.java
+++ src/control-flow/DoWhileDemo.java
@@ -1,12 +1,10 @@
-package Control_Statements;
-
 public class DoWhileDemo {
     public static void main(String[] args) {
 
         int i = 1;
 
         do {
-            System.out.println("i = " + i);
+            System.out.println("i = " + i); 
             i++;
         } while (i <= 5);
     }
