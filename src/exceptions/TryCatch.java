3796b7a871c30e7e64bacbad63b17a6bcc940b87
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-07 15:51:27 +0530

    Try Catch code

--- src/exceptions/TryCatch.java
+++ src/exceptions/TryCatch.java
@@ -2,7 +2,8 @@ public class TryCatch {
     public static void main(String[] args) {
         try {
             int result = 10 / 0;
-        } catch(ArithmeticException e) {
+            System.out.println(result);
+        } catch (ArithmeticException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
