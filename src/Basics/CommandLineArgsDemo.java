932d26804f1a5e10366abe7dea23bf72a30e7878
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-07 16:10:30 +0530

    Final code

--- src/Basics/CommandLineArgsDemo.java
+++ src/Basics/CommandLineArgsDemo.java
@@ -1,3 +1,5 @@
+package basics;
+
 public class CommandLineArgsDemo {
     public static void main(String[] args) { 
 
--- src/Basics/FinalKeywordDemo.java
+++ src/Basics/FinalKeywordDemo.java
@@ -1,12 +1,12 @@
 package basics;
 
 public class FinalKeywordDemo {
-
+ 
     public static void main(String[] args) {
         final int MAX_VALUE = 100;
         System.out.println("Max Value: " + MAX_VALUE);
 
         // MAX_VALUE = 200; // Compile-time error
-    }
+    } 
 }
 
