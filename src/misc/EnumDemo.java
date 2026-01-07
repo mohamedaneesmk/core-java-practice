7eccd928b5585336ee8b723a4a0f695d4cdf8d72
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-07 15:58:46 +0530

    Enum code

---
+++ src/misc/EnumDemo.java
@@ -0,0 +1,9 @@
+package misc;
+
+enum Day { MONDAY, TUESDAY, WEDNESDAY }
+public class EnumDemo {
+    public static void main(String[] args) {
+        Day d = Day.MONDAY;
+        System.out.println("Day: " + d);
+    }
+}
