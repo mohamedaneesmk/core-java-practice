fe642da471b3722fd15e506c6ab5fa34a5594b10
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-07 16:03:09 +0530

    For Loop code

--- src/control-flow/ForLoopDemo.java
+++ src/control-flow/ForLoopDemo.java
@@ -1,10 +1,8 @@
-package Control_Statements;
-
 public class ForLoopDemo {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
-    }
+    } 
 }
