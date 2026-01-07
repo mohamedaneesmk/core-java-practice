5f287534bdade63f44303a1fa52c7e1b84cdc2c9
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-07 15:57:54 +0530

    Stream API code

---
+++ src/java-8-features/StreamAPI.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.stream.*;
+public class StreamAPI {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        List<Integer> squares = numbers.stream()
+                                       .map(n -> n*n)
+                                       .collect(Collectors.toList());
+        System.out.println(squares);
+    }
+}
